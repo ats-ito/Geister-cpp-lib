b37cf67d7c63e815516b6f23e64c36696fd737bf
ats-ito <ats_ito@outlook.com>
2019-07-04 22:30:18 +0900

    対戦用スクリプト改良
    出力量指定機能追加
    勝敗を内訳別に出力するようにした

--- pytools/competition.py
+++ pytools/competition.py
@@ -7,36 +7,48 @@ import execute
 import argparse
 import logging
 import sys
+import cProfile
+# import numba
 
-
-def match(first, second, output=2, log=False):
-    # first.send("init")
-    # second.send("init")
-    game = geister.Geister()
-    red1 = first.send("red")#.upper()
-    game.setRed(red1)
-    game.changeSide()
-    red2 = second.send("red")#.lower()
-    game.setRed(red2)
-    game.changeSide()
-    game.printBoard()
-    result = 0
-    while(result == 0):
-        game.printBoard()
-        hand = first.send("hand " + game.toString())
-        print(hand)
-        game.move(hand[0], hand[4])
-        game.printBoard()
-        result = game.checkResult()
-        if(result):
-            break
+# @numba.jit
+def match(first, second, logEnable, outputLevel, matchCount):
+    results = []
+    for cnt in range(matchCount):
+        # first.send("init")
+        # second.send("init")
+        game = geister.Geister()
+        red1 = first.send("red")#.upper()
+        game.setRed(red1)
         game.changeSide()
-        hand = second.send("hand " + game.toString())
-        print(hand)
-        game.move(hand[0], hand[4])
+        red2 = second.send("red")#.lower()
+        game.setRed(red2)
         game.changeSide()
-        result = game.checkResult()
-    print(result)
+        if outputLevel > 2:
+            game.printBoard()
+        result = 0
+        while(result == 0):
+            if outputLevel > 2:
+                game.printBoard()
+            hand = first.send("hand " + game.toString())
+            if outputLevel > 1:
+                print(hand)
+            game.move(hand[0], hand[4])
+            if outputLevel > 2:
+                game.printBoard()
+            result = game.checkResult()
+            if(result):
+                break
+            game.changeSide()
+            hand = second.send("hand " + game.toString())
+            if outputLevel > 1:
+                print(hand)
+            game.move(hand[0], hand[4])
+            game.changeSide()
+            result = game.checkResult()
+        if outputLevel > 0:
+            print("{}: {}".format(cnt, result))
+        results.append(result)
+    return results
 
 
 if __name__ == "__main__":
@@ -45,12 +57,20 @@ if __name__ == "__main__":
     parser.add_argument('player1')
     parser.add_argument('player2')
     parser.add_argument('-c', '--count', type=int, default=1)
+    parser.add_argument('-o', '--output', type=int, default=2, choices=[0,1,2,3])
+    parser.add_argument('-v', '--version')
+    parser.add_argument('-l', '--log', action='store_true')
 
     args = parser.parse_args()
     first = execute.Execute([args.player1])
     second = execute.Execute([args.player2])
     print("1st {}".format(first.recieve()))
     print("2nd {}".format(second.recieve()))
-    match(first, second)
+    results = match(first, second, logEnable=args.log, outputLevel=args.output, matchCount=args.count)
+    # print(results)
+    resultList = {1:0,2:0,3:0,-1:0,-2:0,-3:0,0:0}
+    for r in results:
+        resultList[r] += 1
+    print(resultList)
     first.send("exit")
     second.send("exit")
