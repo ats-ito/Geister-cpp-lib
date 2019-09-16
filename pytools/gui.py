b1b767656dbddf53b16f36bd50316eab68049225
ats-ito <ats_ito@outlook.com>
2019-09-16 15:32:14 +0900

    棋譜ビューアスクリプト作成
    最低限ファイルを開いて棋譜と盤面を見る機能は実装

--- pytools/gui.py
+++ pytools/gui.py
@@ -2,42 +2,153 @@
 # -*- coding: utf-8 -*-
 from tkinter import *
 from tkinter import ttk
+from tkinter import filedialog
 import geister
 
+kifu = []
+with open('log.txt', 'r') as f:
+    kifu = f.readlines()
+    kifu = kifu[:-1]
+
 lb = None
+v1 = None
+canvas = None
+img = None
+blue1 = None
+red1 = None
+blue2 = None
+red2 = None
+root = None
+
+state_index = 0
+
+game = None
+
+def show_state(state):
+    canvas.delete('all')
+    canvas.create_image(200,250,image=img)
+    for i in range(16):
+        x = int(state[i*3])
+        y = int(state[i*3+1])
+        xPos = x*54+65
+        yPos = y*54+115
+        if state[i*3+2] == 'B':
+            if x < 6:
+                canvas.create_image(xPos, yPos, image=blue1)
+            elif x == 8:
+                canvas.create_image(-54+65, 115, image=blue1)
+        if state[i*3+2] == 'R':
+            if x < 6:
+                canvas.create_image(xPos, yPos, image=red1)
+            elif x == 8:
+                canvas.create_image(-54+65, yPos, image=red1)
+        if state[i*3+2] == 'b':
+            if x < 6:
+                canvas.create_image(xPos, yPos, image=blue2)
+            elif x == 8:
+                canvas.create_image(-54+65, yPos, image=blue2)
+        if state[i*3+2] == 'r':
+            if x < 6:
+                canvas.create_image(xPos, yPos, image=red2)
+            elif x == 8:
+                canvas.create_image(-54+65, yPos, image=red2)
+    print(state)
 
 def button_click():
-    show_selection()
+    # show_selection()
+    # canvas.create_line(1,1,100,100)
+    x = root.winfo_pointerx()
+    y = root.winfo_pointery()
+    abs_coord_x = root.winfo_pointerx() - root.winfo_vrootx()
+    abs_coord_y = root.winfo_pointery() - root.winfo_vrooty()
+    wx, wy = x - root.winfo_x(), y - root.winfo_y()
+    print('{}, {}. {}, {}. {}, {}'.format(x, y, abs_coord_x, abs_coord_y, wx, wy))
     
 def listbox_selected(event):
     show_selection()
+    state = lb.get(lb.curselection()[0]).rstrip('\n').split(',')[-1]
+    show_state(state)
     
 def show_selection():
     for i in lb.curselection():
-        print(lb.get(i))
+        print(i, end=': ')
+        print(lb.get(i).rstrip('\n'))
+
+def donothing():
+    pass
+
+def openFile():
+    global v1
+    fn = filedialog.askopenfilename()#filetypes = typ, initialdir = dir)
+    # print(fn)
+    kifu = []
+    with open(fn, 'r') as f:
+        kifu = f.readlines()
+        kifu = kifu[:-1]
+    v1.set(kifu)
+    # lb.update()
+    # print(v1)
+
+def motion(event):
+    x, y = event.x, event.y
+    print('{}, {}'.format(x, y))
 
 def run():
     global lb
+    global v1
+    global canvas
+    global img
+    global blue1
+    global red1
+    global blue2
+    global red2
+    global root
     root = Tk()
-    root.title('Listbox 1')
+    root.title('GUI for Geister')
     root.geometry('800x600')
+
+    img = PhotoImage(file='./board.png')
+    blue1 = PhotoImage(file='./blue1.png')
+    red1 = PhotoImage(file='./red1.png')
+    blue2 = PhotoImage(file='./blue2.png')
+    red2 = PhotoImage(file='./red2.png')
+    canvas = Canvas(root, bg="white", height=500, width=400)
+    state = kifu[2].rstrip('\n').split(',')[-1]
+    show_state(state)
+    # label1 = Label(root, image=img, width=200)
+    # label1.pack(side='left', ipadx=50, expand=True)
+    # canvas.bind('<Button-1>', motion)
+    canvas.pack(side='left', expand=True)
     
     # フレーム
     frame1 = ttk.Frame(root, padding=10)
-    frame1.grid()
+    frame1.pack(side='right')
     
     # リストボックス
-    currencies = ('JPY', 'USD')
-    v1 = StringVar(value=currencies)
-    lb = Listbox(frame1, listvariable=v1,height=4)
-    lb.insert(END, 'EUR')
+    v1 = StringVar(value=kifu)
+    lb = Listbox(frame1, listvariable=v1,height=30,width=40)
     lb.bind('<<ListboxSelect>>', listbox_selected)
     lb.grid(row=0, column=0)
-    
+
     #Button
-    button1 = ttk.Button(frame1, text='OK', command=button_click)
+    button1 = ttk.Button(frame1, text='OK', command=button_click, cursor="plus")
     button1.grid(row=0, column=1)
+
+    menubar = Menu(root)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="Open", command=openFile)
+    filemenu.add_command(label="Save", command=donothing)
+    filemenu.add_command(label="Save as...", command=donothing)
+    filemenu.add_command(label="Close", command=donothing)
+    filemenu.add_command(label="Exit", command=root.quit)
+    menubar.add_cascade(label="File", menu=filemenu)
+    filemenu = Menu(menubar, tearoff=0)
+    filemenu.add_command(label="New", command=donothing)
+    menubar.add_cascade(label="Game", menu=filemenu)
+
+    # root.bind('<Motion>', motion)
     
+    root.config(menu=menubar)
     root.mainloop()
 
 if __name__ == '__main__':
